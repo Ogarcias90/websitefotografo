a9900fe5779433a6bf792bfdefaf6380cec4959f
Ogarcias90 <84973872+Ogarcias90@users.noreply.github.com>
2022-07-02 20:12:54 -0300

    estructurabasica
    Co-Authored-By: Luciano Pardo <pardobio@gmail.com>

---
+++ src/components/Carrousel/carrousel.js
@@ -0,0 +1,11 @@
+import "./carrousel.css";
+
+function Carrousel() {
+  return (
+    <div className="Carrousel">
+      <h1>"Este es el carrousel de imagenes"</h1>
+    </div>
+  );
+}
+
+export default Carrousel;
