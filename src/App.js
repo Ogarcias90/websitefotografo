ff9137165546e8cd1c94830044f6fb52c49005f4
Luciano Pardo <pardobio@gmail.com>
2022-07-16 21:37:17 -0300

    Comienzo de la era oscura

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
+
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Carrousel from "./components/Carrousel/carrousel";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+
+  const changeClassName = (id,name1,name2) =>{
+    let cName = document.getElementById(id);
+    cName.className = (cName.className === name2) ? name1 : name2;
+  };
+
+  const darkMode = () => {
+    changeClassName('body','lightBody','darkBody')
+    changeClassName('header','lightHeader','darkHeader')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header id='header' className="lightHeader">
         <h1>Oscar Garcias Fotografía </h1>
-        <nav>
-          <ul>
-            <li>Inicio</li>
-            <li>Blog</li>
-            <li>Galeria</li>
-          </ul>
-        </nav>
+        
       </header>
-      <body>
+      <body id='body' className="lightBody">
+        <NavBar/>
         <Carrousel />
+        <div className="toggleCont">
+            <div className="toggle-switch">
+                <input 
+                    type="checkbox" 
+                    className="checkbox" 
+                    name='toggle' 
+                    id='toggle'
+                    onInput={darkMode}
+                />
+                <label className="label" htmlFor='toggle'>
+                    <span className="inner" />
+                    <span className="switch" />
+                </label>
+            </div>
+        </div>
       </body>
       <footer>
         <div className="socialMedia">
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+
+import React, {useState} from 'react';
+import "./NavBar.css";
+
+function NavBar() {
+
+
+  return (
+    <nav>
+        <ul>
+            <li>Inicio</li>
+            <li>Blog</li>
+            <li>Galeria</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
